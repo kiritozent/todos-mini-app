65e7dc3735083c38b7a50ee8f61146bc26bcdeec
muhammad iqbal <muhammadiqbalks2@gmail.com>
2020-02-29 22:10:46 +0700

    [UPDATE]: refactoring store to immutable objects

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,8 +1,9 @@
 import _ from 'lodash';
+import Immutable from 'seamless-immutable';
 import types from './actionTypes';
 import { DEFAULT_REDUCERS } from '../data/const';
 
-const initialState = {
+const initialState = Immutable({
   getUsers: DEFAULT_REDUCERS,
   getTodos: DEFAULT_REDUCERS,
   postTodos: DEFAULT_REDUCERS,
@@ -10,130 +11,129 @@ const initialState = {
   deleteTodos: DEFAULT_REDUCERS,
   userList: [],
   todoList: [],
-};
+});
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     // getUsers reducers
     case types.GET_USERS_REQUEST: {
-      const newState = {
+      return state.merge({
         ...state,
         getUsers: { payload: null, fetching: true, error: false },
-      };
-      return newState;
+      });
     }
     case types.GET_USERS_SUCCESS: {
       const { payload } = action;
-      return {
+      return state.merge({
         ...state,
         getUsers: { payload, fetching: false, error: false },
         userList: payload.data,
-      };
+      });
     }
     case types.GET_USERS_FAILURE:
-      return {
+      return state.merge({
         ...state,
         getUsers: { payload: null, fetching: false, error: true },
-      };
+      });
 
     // getTodos reducers
     case types.GET_TODO_LIST_REQUEST:
-      return {
+      return state.merge({
         ...state,
         getTodos: { payload: null, fetching: true, error: false },
-      };
+      });
     case types.GET_TODO_LIST_SUCCESS: {
       const { payload } = action;
-      return {
+      return state.merge({
         ...state,
         getTodos: { payload, fetching: false, error: false },
         todoList: payload.data,
-      };
+      });
     }
     case types.GET_TODO_LIST_FAILURE:
-      return {
+      return state.merge({
         ...state,
         getTodos: { payload: null, fetching: false, error: true },
-      };
+      });
 
     // postTodos reducers
     case types.POST_TODO_LIST_REQUEST:
-      return {
+      return state.merge({
         ...state,
         postTodos: { payload: null, fetching: true, error: false },
-      };
+      });
     case types.POST_TODO_LIST_SUCCESS: {
       const { payload } = action;
       const temp = [...state.todoList];
       temp.push(payload.data);
       const result = _.sortBy(temp, 'id');
 
-      return {
+      return state.merge({
         ...state,
         postTodos: { payload, fetching: false, error: false },
         todoList: result,
-      };
+      });
     }
     case types.POST_TODO_LIST_FAILURE:
-      return {
+      return state.merge({
         ...state,
         postTodos: { payload: null, fetching: false, error: true },
-      };
+      });
 
     // postTodos reducers
     case types.PATCH_TODO_LIST_REQUEST:
-      return {
+      return state.merge({
         ...state,
         patchTodos: { payload: null, fetching: true, error: false },
-      };
+      });
     case types.PATCH_TODO_LIST_SUCCESS: {
       const { payload } = action;
 
       const result = _.unionBy(state.todoList, [payload.data], 'id');
 
-      return {
+      return state.merge({
         ...state,
         patchTodos: { payload, fetching: false, error: false },
         todoList: result,
-      };
+      });
     }
     case types.PATCH_TODO_LIST_FAILURE:
-      return {
+      return state.merge({
         ...state,
         patchTodos: { payload: null, fetching: false, error: true },
-      };
+      });
 
     // postTodos reducers
     case types.DELETE_TODO_LIST_REQUEST:
-      return {
+      return state.merge({
         ...state,
         patchTodos: { payload: null, fetching: true, error: false },
-      };
+      });
     case types.DELETE_TODO_LIST_SUCCESS: {
       const { payload } = action;
 
       let result = [...state.todoList];
 
-      const deletedIndex = temp.findIndex(item => item.id === payload.id);
+      const deletedIndex = result.findIndex(item => item.id === payload.id);
 
       if (deletedIndex >= 0) {
-        result = temp.splice(deletedIndex, 1);
+        result = result.splice(deletedIndex, 1);
       }
 
-      return {
+      return state.merge({
         ...state,
         patchTodos: { payload, fetching: false, error: false },
         todoList: result,
-      };
+      });
     }
     case types.DELETE_TODO_LIST_FAILURE:
-      return {
+      return state.merge({
         ...state,
         patchTodos: { payload: null, fetching: false, error: true },
-      };
+      });
 
     default:
-      return { ...state };
+      return state;
   }
 };
 export { initialState, reducer };
